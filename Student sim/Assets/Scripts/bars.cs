0b94414923cf158ecb741602c8aab0d7c8291c8e
TimGoedhuys <32095184+TimGoedhuys@users.noreply.github.com>
2018-02-22 12:31:15 +0100

    bars

---
+++ Student sim/Assets/Scripts/bars.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class bars : MonoBehaviour {
+
+    public GameObject SocialBar;
+    private float barlength;
+	// Use this for initialization
+	void Start () {
+        barlength = 100;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
