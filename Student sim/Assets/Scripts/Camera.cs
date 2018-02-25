7f5392b91c5d9e725631c6dc13c26a0ec23dac9e
Timo Lapre <t.lapre@students.uu.nl>
2018-02-25 17:59:34 +0100

    improved controls

---
+++ Student sim/Assets/Scripts/Camera.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Camera : MonoBehaviour {
+
+    public Player player;
+    [Range(1,6)]
+    public int Offset;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.localPosition = new Vector3(-player.transform.position.x/Offset,2,-2);
+	}
+}
--- Student sim/Assets/Scripts/Player.cs
+++ Student sim/Assets/Scripts/Player.cs
@@ -4,28 +4,55 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
 
-    public int Jump;
+    [Range(200, 300)]
+    public int JumpForce; 
+    [Range(0,25)]
+    public int HorizontalSpeed;
+    public int GameSpeed, GameRotation;
 
-    private int Pos;
+    private bool OnFloor;
+    private float MoveTime;
+
+    public int Pos;
     private Vector2 MousePos, ConstantMousePos;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        if (Swipe() == "Right" && Pos < 1)
+    private KeyCode[] RightKeys = new KeyCode[] { KeyCode.RightArrow, KeyCode.D };
+    private KeyCode[] LeftKeys = new KeyCode[] { KeyCode.LeftArrow, KeyCode.A };
+    private KeyCode[] JumpKeys = new KeyCode[] { KeyCode.Space, KeyCode.UpArrow, KeyCode.W };
+
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+        transform.Translate(Vector3.forward * Time.deltaTime * GameSpeed);
+
+        transform.eulerAngles = Vector3.zero;
+
+        if ((Swipe() == "Right" || KeyPressed("Right")) && Pos < 1)
+        {
             Pos++;
-        else if (Swipe() == "Left" && Pos > -1)
+            MoveTime = 0;
+        }
+        else if ((Swipe() == "Left" || KeyPressed("Left")) && Pos > -1)
+        {
             Pos--;
+            MoveTime = 0;
+        }
 
-        if (Swipe() == "Jump")
-            GetComponent<Rigidbody>().AddForce(0,Jump,0);
+        if ((Swipe() == "Jump" || KeyPressed("Jump")) && OnFloor)
+        {
+            GetComponent<Rigidbody>().AddForce(0, JumpForce, 0);
+            OnFloor = false ;
+        }
 
-        transform.position = new Vector3(Pos,transform.position.y,-8);
-	}
+        MoveTime += Time.deltaTime*HorizontalSpeed;
+        transform.position = Vector3.Lerp(transform.position, new Vector3(Pos, transform.position.y, transform.position.z), MoveTime);
+        //transform.position = new Vector3(Pos, transform.position.y, -8);
+    }
 
     private string Swipe()
     {
@@ -34,10 +61,10 @@ public class Player : MonoBehaviour {
             MousePos = Input.mousePosition;
         }
         ConstantMousePos = Input.mousePosition;
-        
+
         if (Input.GetMouseButtonUp(0))
         {
-            if (MousePos.x < ConstantMousePos.x && Mathf.Abs(MousePos.x-ConstantMousePos.x) > Mathf.Abs(MousePos.y-ConstantMousePos.y))
+            if (MousePos.x < ConstantMousePos.x && Mathf.Abs(MousePos.x - ConstantMousePos.x) > Mathf.Abs(MousePos.y - ConstantMousePos.y))
                 return "Right";
             if (MousePos.x > ConstantMousePos.x && Mathf.Abs(MousePos.x - ConstantMousePos.x) > Mathf.Abs(MousePos.y - ConstantMousePos.y))
                 return "Left";
@@ -48,10 +75,41 @@ public class Player : MonoBehaviour {
         return null;
     }
 
-    void OnCollisionEnter(Collision collision)
+    void OnTriggerEnter(Collider collision)
     {
         if (collision.transform.tag == "Obstacle")
             Debug.Log("HIT");
+
+    }
+
+    void OnCollisionEnter(Collision coll)
+    {
+        if (coll.transform.tag == "Floor")
+            OnFloor = true;
+    }
+
+    bool KeyPressed(string move)
+    {
+        if(move == "Right")
+            for (int i = 0; i < RightKeys.Length; i++)
+            {
+                if (Input.GetKeyDown(RightKeys[i]))
+                    return true;
+            }
+        if (move == "Left")
+            for (int i = 0; i < LeftKeys.Length; i++)
+            {
+                if (Input.GetKeyDown(LeftKeys[i]))
+                    return true;
+            }
+        if (move == "Jump")
+            for (int i = 0; i < JumpKeys.Length; i++)
+            {
+                if (Input.GetKeyDown(JumpKeys[i]))
+                    return true;
+            }
+
+        return false;
     }
 
 }
