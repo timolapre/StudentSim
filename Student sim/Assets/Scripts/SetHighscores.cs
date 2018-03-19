1b68786091daaf8a7491c46309b1531210fc17e4
TimGoedhuys <32095184+TimGoedhuys@users.noreply.github.com>
2018-03-19 20:03:16 +0100

    kaasdata?! lol

---
+++ Student sim/Assets/Scripts/SetHighscores.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using UnityEngine;
+
+public class SetHighscores : MonoBehaviour {
+
+    string Output;
+    public void SetHS()
+    {
+        StreamWriter writer = new StreamWriter(@"C:assets\Highscores", true);
+        Output = PlayerPrefs.GetFloat("Highscore").ToString();
+        writer.WriteLine(Output);
+        writer.Close();
+    }
+}
