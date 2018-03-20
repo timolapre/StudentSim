39d7322fe3c25d6fcc39ce37e43c7f44239c6354
TimGoedhuys <32095184+TimGoedhuys@users.noreply.github.com>
2018-03-20 17:13:50 +0100

    asd

--- Student sim/Assets/Scripts/SetHighscores.cs
+++ Student sim/Assets/Scripts/SetHighscores.cs
@@ -2,15 +2,40 @@
 using System.Collections.Generic;
 using System.IO;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class SetHighscores : MonoBehaviour {
 
     string Output;
+    public GameObject can1, Highscores;
+    public InputField Name;
+    string Text;
+    int Lines;
+    private int i;
+
+    private void Start()
+    {
+        Highscores.SetActive(false);
+    }
+
     public void SetHS()
     {
         StreamWriter writer = new StreamWriter(@"C:assets\Highscores", true);
-        Output = PlayerPrefs.GetFloat("Highscore").ToString();
+        Output = PlayerPrefs.GetFloat("Highscore").ToString() + ":" + Name.text;
         writer.WriteLine(Output);
         writer.Close();
+        can1.SetActive(false);
+        Highscores.SetActive(true);
+    }
+
+    public void ReadDB()
+    {
+        StreamReader Reader = new StreamReader(@"C:assets\Highscores", true);
+        Lines = File.ReadAllLines(@"C:assets\Highscores").Length;
+        do
+        {
+        }
+        while (Text != null);
+        Reader.Close();
     }
 }
