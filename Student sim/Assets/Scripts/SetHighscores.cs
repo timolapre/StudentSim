902dff77d4909e2b80f3c4e8e0ca0e7117345eb0
TimGoedhuys <32095184+TimGoedhuys@users.noreply.github.com>
2018-03-20 19:05:11 +0100

    highscore werkt volledig, zet ze alleen niet op volgorde...

--- Student sim/Assets/Scripts/SetHighscores.cs
+++ Student sim/Assets/Scripts/SetHighscores.cs
@@ -7,15 +7,17 @@ using UnityEngine.UI;
 public class SetHighscores : MonoBehaviour {
 
     string Output;
-    public GameObject can1, Highscores;
+    public GameObject can1, Highscores, TextField;
+    private GameObject Field;
     public InputField Name;
     string Text;
-    int Lines;
-    private int i;
+    string[] Loaded;
+    int Lines, j;
 
     private void Start()
     {
         Highscores.SetActive(false);
+        j = 0;
     }
 
     public void SetHS()
@@ -30,10 +32,18 @@ public class SetHighscores : MonoBehaviour {
 
     public void ReadDB()
     {
+        List<string> HSlist = new List<string>();
         StreamReader Reader = new StreamReader(@"C:assets\Highscores", true);
-        Lines = File.ReadAllLines(@"C:assets\Highscores").Length;
         do
         {
+            Text = Reader.ReadLine();
+            Loaded = Text.Split(':');
+            Field = Instantiate(TextField, new Vector3(0,0,0), transform.rotation);
+            Text TheText = Field.transform.GetComponent<Text>();
+            TheText.text = Loaded[0] + "    " + Loaded[1];
+            Field.transform.parent = Highscores.transform;
+            Field.transform.position = new Vector3(300, 250 - 20 * j, 0);
+            j++;
         }
         while (Text != null);
         Reader.Close();
